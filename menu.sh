202eb7d3a9c66689e6c8bdf2d3653a47601867d4
michaelBedDev <micama2000@gmail.com>
2024-01-25 13:48:48 +0100

    Plantilla menu

---
+++ menu.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+`clear`
+
+echo "Introduce la opción deseada. [1-5]"
+echo "Pulsa 99 para salir"
+read opcion
+
+case $opcion in
+    1)
+    #accion
+    ;;
+    2)
+    #accion
+    ;;
+    3)
+    #accion
+    ;;
+    4)
+    #accion
+    ;;
+    5)
+    #accion
+    ;;
+    99)
+    exit
+    ;;
+esac
