6497c5eaf09e48aaeb816bb64d9cd9d93ee93bf9
michaelBedDev <micama2000@gmail.com>
2024-01-28 20:27:42 +0100

    Ejercicio Sistemas Finalizado?

--- menu.sh
+++ menu.sh
@@ -1,41 +1,65 @@
 #!/bin/bash
 clear
 echo "Introduce la opción deseada. [1-5]"
-echo "Pulsa 99 para salir"
-read opcion
+read -p "Pulsa 99 para salir" opcion
 
-case $opcion in
-    1)
-    CrearFichero
-    ;;
-    2)
-    #accion
-    ;;
-    3)
-    #accion
-    ;;
-    4)
-    #accion
-    ;;
-    5)
-    #accion
-    ;;
-    99)
-    exit
-    ;;
-esac
+salir = false
+while ((salir == false))
+do
+    case $opcion in
+        1)
+        crearFichero
+        ;;
+        2)
+        eliminarFichero
+        ;;
+        3)
+        listarContenido
+        ;;
+        4)
+        date
+        ;;
+        5)
+        xdg-open "https://t.ly/nw-Kg" >/dev/null 2>&1
+        sleep 5
+        ;;
+        99)
+        salir = true
+        ;;
+    esac
+done
 
-function CrearFichero () {
-            mkdir directorio1
-            cd directorio1
-            echo "Creado archivo de prueba, y destruido en 5 segundos." > archivo1.txt
-            cat archivo1.txt
-            for((i=5; i>=0; i--))
-            do
-                echo $i
-                sleep 1
-            done
-            cd ..
-            rm -rf directorio1
+function crearFichero () {
+        mkdir directorio1
+        cd directorio1
+        echo "Creado archivo de prueba, y destruido en 5 segundos." > archivo1.txt
 
+}
+
+function eliminarFichero () {
+        cd directorio1
+        cat archivo1.txt
+        for((i=5; i>=0; i--))
+        do
+             echo $i
+            sleep 1
+        done
+        cd ..
+        rm -rf directorio1
+}
+
+
+function listarContenido () {
+        cd ~
+        ls -la
+}
+
+
+function mostrarOpciones(){
+    echo "1. Crear un directorio y un archivo de texto dentro del mismo."
+    echo "2. Mostrar el contenido del archivo de texto."
+    echo "3. Listar el contenido del directorio principal del usuario"
+    echo "4. Mostrar fecha de hoy"
+    echo "5. Abrir el navegador para comprar una placa base."
+    echo "99. Salir"
 }
