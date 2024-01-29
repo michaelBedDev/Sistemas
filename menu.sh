fa8208bd4f8f1854bd90c2043ec28367e0a590ab
michaelBedDev <micama2000@gmail.com>
2024-01-29 10:48:02 +0100

    FINISHED

--- menu.sh
+++ menu.sh
@@ -1,5 +1,38 @@
 #!/bin/bash
 clear
+
+crearFichero () {
+    mkdir directorio1
+    cd directorio1
+    echo "Creado archivo de prueba" > archivo1.txt
+    cat archivo1.txt
+}
+
+eliminarFichero () {
+    cd ..
+    echo "Eliminando directorio1 y todo lo que contiene"
+    for((i=5; i>=0; i--))
+    do
+        echo $i
+        sleep 1
+    done
+    rm -rf directorio1
+}
+
+listarContenido () {
+    cd ~
+    ls -la
+}
+
+mostrarOpciones () {
+    echo "1. Crear un directorio y un archivo de texto dentro del mismo."
+    echo "2. Mostrar el contenido del archivo de texto."
+    echo "3. Listar el contenido del directorio principal del usuario"
+    echo "4. Mostrar fecha de hoy"
+    echo "5. Abrir el navegador para comprar una placa base."
+    echo "99. Salir"
+}
+
 mostrarOpciones
 opcion=0
 
@@ -23,42 +56,10 @@ do
         ;;
         5)
         xdg-open "https://t.ly/nw-Kg" >/dev/null 2>&1
-        sleep 5
+        sleep 2
         ;;
         99)
         echo "Hasta luego!"
         ;;
     esac
 done
-function crearFichero () {
-    mkdir directorio1
-    cd directorio1
-    echo "Creado archivo de prueba" > archivo1.txt
-    cat archivo1.txt
-}
-
-function eliminarFichero () {
-    cd ..
-    echo "Eliminando directorio1 y todo lo que contiene"
-    for((i=5; i>=0; i--))
-    do
-        echo $i
-        sleep 1
-    done
-    rm -rf directorio1
-}
-
-
-function listarContenido () {
-    cd ~
-    ls -la
-}
-
-function mostrarOpciones () {
-    echo "1. Crear un directorio y un archivo de texto dentro del mismo."
-    echo "2. Mostrar el contenido del archivo de texto."
-    echo "3. Listar el contenido del directorio principal del usuario"
-    echo "4. Mostrar fecha de hoy"
-    echo "5. Abrir el navegador para comprar una placa base."
-    echo "99. Salir"
-}
